076e6b9fecebfb80c28cb56bf21aebba6c4389e9
MariaBetancur <mabo6241@gmail.com>
2023-06-12 18:51:54 -0500

    v1.5

--- src/api.js
+++ src/api.js
@@ -1,59 +1,67 @@
-export function fetchForm(searchQuery) {
-  return fetch(
-    `https://pixabay.com/api/?key=37145039-d4ad8d6ab2b85cf5d231e1aa0&q=${encodeURIComponent(
-      searchQuery
-    )}&image_type=photo&orientation=horizontal&safesearch=true`,
-    {
-      headers: {
-        'x-api-key': '37145039-d4ad8d6ab2b85cf5d231e1aa0',
-      },
-    }
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-    return response.json();
-  });
-}
-
-export function fetchPhotocard(photocardId) {
-  return fetch(
-    `https://pixabay.com/api/?key=37145039-d4ad8d6ab2b85cf5d231e1aa0&id=${photocardId}`,
-    {
-      headers: {
-        'x-api-key': '37145039-d4ad8d6ab2b85cf5d231e1aa0',
-      },
-    }
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
+export async function fetchForm(searchQuery) {
+  try {
+    const response = await fetch(
+      `https://pixabay.com/api/?key=37145039-d4ad8d6ab2b85cf5d231e1aa0&q=${encodeURIComponent(
+        searchQuery
+      )}&image_type=photo&orientation=horizontal&safesearch=true`,
+      {
+        headers: {
+          'x-api-key': '37145039-d4ad8d6ab2b85cf5d231e1aa0',
+        },
       }
-      return response.json();
-    })
-    .then(data => data[0]);
-}
-
-export function fetchLoadmore(searchQuery, page) {
-  return fetch(
-    `https://pixabay.com/api/?key=37145039-d4ad8d6ab2b85cf5d231e1aa0&q=${encodeURIComponent(
-      searchQuery
-    )}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
-    {
-      headers: {
-        'x-api-key': '37145039-d4ad8d6ab2b85cf5d231e1aa0',
-      },
-    }
-  ).then(response => {
+    );
     if (!response.ok) {
       throw new Error(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     }
     return response.json();
-  });
+  } catch (error) {
+    throw new Error('An error occurred while fetching the form data.');
+  }
+}
+
+export async function fetchPhotocard(photocardId) {
+  try {
+    const response = await fetch(
+      `https://pixabay.com/api/?key=37145039-d4ad8d6ab2b85cf5d231e1aa0&id=${photocardId}`,
+      {
+        headers: {
+          'x-api-key': '37145039-d4ad8d6ab2b85cf5d231e1aa0',
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    const data = await response.json();
+    return data[0];
+  } catch (error) {
+    throw new Error('An error occurred while fetching the photocard data.');
+  }
+}
+
+export async function fetchLoadmore(searchQuery, page) {
+  try {
+    const response = await fetch(
+      `https://pixabay.com/api/?key=37145039-d4ad8d6ab2b85cf5d231e1aa0&q=${encodeURIComponent(
+        searchQuery
+      )}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+      {
+        headers: {
+          'x-api-key': '37145039-d4ad8d6ab2b85cf5d231e1aa0',
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    return response.json();
+  } catch (error) {
+    throw new Error('An error occurred while fetching the loadmore data.');
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as Notiflix from 'notiflix';
+import { fetchForm, fetchPhotocard, fetchLoadmore } from './api';
 
 const searchForm = document.querySelector('.search-form');
 const galleryEl = document.querySelector('.gallery');
@@ -8,59 +9,59 @@ const fetchLoadMoreBtn = document.querySelector('.load-more');
 let page = 1;
 const perPage = 40;
 
-fetchForm()
-  .then(form => {
+(async () => {
+  try {
     loader.classList.add('hidden');
+    const form = await fetchForm();
     console.log(form);
     const options = [];
     form.forEach(form => {});
     const breedSelect = document.querySelector('.breed-select');
     breedSelect.append(...options);
-  })
-  .catch(error => {
+  } catch (error) {
     console.error('Error:', error);
     errorElement.classList.remove('hidden');
-  });
+  }
+})();
 
-searchForm.addEventListener('change', e => {
+searchForm.addEventListener('change', async e => {
   const selectedOption = e.target.value;
   if (selectedOption) {
     loader.classList.remove('hidden');
-    fetchPhotocard(selectedOption)
-      .then(photoCardData => {
-        loader.classList.add('hidden');
-        console.log(photoCardData);
-        const galleryData = photoCardData.gallery[0];
-        galleryEl.innerHTML = `
-          <div class="photo-card">
-            <img src="${photoCardData.largeImageURL}" alt="${photoCardData.tags}" loading="lazy" />
-            <div class="info">
-              <p class="info-item">
-                <b>Likes:</b> ${photoCardData.likes}
-              </p>
-              <p class="info-item">
-                <b>Views:</b> ${photoCardData.views}
-              </p>
-              <p class="info-item">
-                <b>Comments:</b> ${photoCardData.comments}
-              </p>
-              <p class="info-item">
-                <b>Downloads:</b> ${photoCardData.downloads}
-              </p>
-            </div>
-          </div>`;
-        loader.classList.add('hidden');
-        galleryEl.classList.remove('hidden');
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        Notiflix.Notify.failure('An error occurred. Please try again later.');
-        errorElement.classList.remove('hidden');
-      });
+    try {
+      const photoCardData = await fetchPhotocard(selectedOption);
+      loader.classList.add('hidden');
+      console.log(photoCardData);
+      const galleryData = photoCardData.gallery[0];
+      galleryEl.innerHTML = `
+        <div class="photo-card">
+          <img src="${photoCardData.largeImageURL}" alt="${photoCardData.tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes:</b> ${photoCardData.likes}
+            </p>
+            <p class="info-item">
+              <b>Views:</b> ${photoCardData.views}
+            </p>
+            <p class="info-item">
+              <b>Comments:</b> ${photoCardData.comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads:</b> ${photoCardData.downloads}
+            </p>
+          </div>
+        </div>`;
+      loader.classList.add('hidden');
+      galleryEl.classList.remove('hidden');
+    } catch (error) {
+      console.error('Error:', error);
+      Notiflix.Notify.failure('An error occurred. Please try again later.');
+      errorElement.classList.remove('hidden');
+    }
   }
 });
 
-fetchLoadMoreBtn.addEventListener('click', () => {
+fetchLoadMoreBtn.addEventListener('click', async () => {
   loader.classList.add('hidden');
   if (page > totalHits) {
     Notiflix.Notify.info(
@@ -70,18 +71,17 @@ fetchLoadMoreBtn.addEventListener('click', () => {
     return;
   }
 
-  fetchLoadmore()
-    .then(loadMore => {
-      renderLoadMore(loadMore);
-      page++;
+  try {
+    const loadMore = await fetchLoadmore();
+    renderLoadMore(loadMore);
+    page++;
 
-      if (page > 1) {
-        fetchLoadMoreBtn.style.display = 'block';
-        fetchLoadMoreBtn.textContent = 'Load more';
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      Notiflix.Notify.failure('An error occurred while loading more images.');
-    });
+    if (page > 1) {
+      fetchLoadMoreBtn.style.display = 'block';
+      fetchLoadMoreBtn.textContent = 'Load more';
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    Notiflix.Notify.failure('An error occurred while loading more images.');
+  }
 });
